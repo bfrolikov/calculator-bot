e902b0bb1883079f5d54b7fa51fb5691e51de5e1
b.frolikov <b.frolikov@yandex.ru>
2019-09-18 20:50:13 +0300

    added confirmation

--- controllers/bot.js
+++ controllers/bot.js
@@ -3,10 +3,10 @@ const config = require('../utils/config');
 
 botRouter.post('/', (request, response) => {
   const data = request.body;
- /* if (data.type === 'confirmation' && data.group_id === config.GROUP_ID)
-    response.send(config.CONFIRMATION_TOKEN);*/
-  console.log(config.CONFIRMATION_TOKEN);
-  response.send('');
+  if (data.type === 'confirmation' && data.group_id === Number(config.GROUP_ID))
+    response.send(config.CONFIRMATION_TOKEN);
+  else
+    response.send('');
 });
 
 botRouter.get('/', (request, response) => {
