ab2a7eeb633a2a698431b061e1411c5283d5c573
b.frolikov <b.frolikov@yandex.ru>
2019-09-19 00:18:56 +0300

    testing

--- utils/axios_sender.js
+++ utils/axios_sender.js
@@ -1,12 +1,20 @@
-const axios = require('axios')
-const config = require('./config')
+const axios = require('axios');
+const httpBuildQuery = require('http-build-query');
+const config = require('./config');
 const messageUrl = 'https://api.vk.com/method/messages.send?';
 
+
 const sendMessage = (userId, message) => {
-  const resultUrl = `${messageUrl}user_id=${userId}&access_token=${config.ACCESS_KEY}&v=${config.VK_VESION}&message=${message}`;
-  console.log(resultUrl);
+  const query = {
+    user_id: userId,
+    access_token: config.ACCESS_KEY,
+    v: config.VK_VESION,
+    message: message
+  };
+  const resultUrl = httpBuildQuery(query);
+  console.log(`${messageUrl}${resultUrl}`);
   return axios
-    .get(resultUrl);
+    .get(`${messageUrl}${resultUrl}`);
 };
 
 module.exports = { sendMessage };
