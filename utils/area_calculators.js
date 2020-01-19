68b342c9887a95395753fdb3b52900e4c5f89b82
b.frolikov <b.frolikov@yandex.ru>
2020-01-19 17:58:40 +0300

    fixed the not replying issue

--- utils/area_calculators.js
+++ utils/area_calculators.js
@@ -37,5 +37,59 @@ const truncatedConeArea = (terms, areaType) => {
     return `${S}π`;
   }
 };
+const truncatedPyramidArea = (terms, areaType) => {
+  const a = Number(terms[2]);
+  const b = Number(terms[3]);
+  const h = Number(terms[4]);
+  if (areaType === 1) { // full
+    const S = a * a + b * b + 2 * (a + b) * h;
+    return `${S}`;
+  }
+  else { // side
+    const S = 2 * (a + b) * h;
+    return `${S}`;
+  }
+};
+const pyramidArea = (terms, areaType) => {
+  const a = Number(terms[2]);
+  const h = Number(terms[3]);
+  const SSidePartial = 3 * a * h;
+  let SSide = '';
+  if (SSidePartial % 2 === 0)
+    SSide = `${SSidePartial / 2}`;
+  else
+    SSide = `${SSidePartial}/2`;
+  if (areaType === 1) { // full
+    const SBottomPartial = a * a;
+    let SBottom = '';
+    if (SBottomPartial % 4 === 0)
+      SBottom = `${SBottomPartial / 4}*sqrt(3)`;
+    else
+      SBottom = `${SBottomPartial}*sqrt(3)/4`;
+    return `${SSide} + ${SBottom}`;
+  }
+  else { // side
+    return SSide;
+  }
+};
+const prismArea = (terms, areaType) => {
+  const a = Number(terms[2]);
+  const h = Number(terms[3]);
+  if (areaType === 1) { // full
+    const S = 4 * a * h + 2 * a * a;
+    return `${S}`;
+  }
+  else { // side
+    const S = 4 * a * h;
+    return `${S}`;
+  }
+};
 
-module.exports = { coneArea, cylinderArea, truncatedConeArea };
+module.exports = {
+  coneArea,
+  cylinderArea,
+  truncatedConeArea,
+  truncatedPyramidArea,
+  pyramidArea,
+  prismArea
+};
